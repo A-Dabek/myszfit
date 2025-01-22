e219388e4659a3509af1c182b664e2125b045f38
Adrian Dabek <adrian950113@gmail.com>
2025-01-22 20:22:09 +0100

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {
   inject,
 } from '@angular/core';
 import { map } from 'rxjs';
+import { HeaderComponent } from './header.component';
 import { ProgressComponent } from './progress/progress.component';
 import { StateService } from './state.service';
 
@@ -19,12 +20,10 @@ export interface ActivityViewModel {
   selector: 'app-root',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgForOf, AsyncPipe, ProgressComponent, NgIf],
+  imports: [NgForOf, AsyncPipe, ProgressComponent, NgIf, HeaderComponent],
   template: `
-    <main class="bg-gray-100 min-h-screen">
-      <h1 class="text-center text-xl font-bold mb-6">
-        Dzisiaj {{ currentDay() }}
-      </h1>
+    <main class="bg-gray-100 min-h-screen pt-3">
+      <app-header class="block" />
       <div class="flex flex-col items-center justify-center">
         <div class="flex flex-col space-y-4">
           <div
@@ -108,14 +107,6 @@ export class AppComponent {
   sliderVisibleId: string | null = null; // Tracks the currently visible slider
   previewingId: string | null = null; // Tracks the activity being previewed
   previewDate: string | null = null; // Holds the preview date for display
-
-  readonly currentDay = computed(() => {
-    return new Date().toLocaleDateString('pl', {
-      weekday: 'long',
-      day: 'numeric',
-      month: 'long',
-    });
-  });
 
   readonly activities$ = this.stateService.activities$.pipe(
     map((activities) =>
---
+++ src/app/header.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-header',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <h1 class="text-center text-xl font-bold mb-6">
+      {{ currentDay() }}
+    </h1>
+  `,
+})
+export class HeaderComponent {
+  readonly currentDay = computed(() => {
+    const today = new Date();
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(
+      today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1),
+    );
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6);
+
+    const startOfWeekFormatted = startOfWeek.toLocaleDateString('pl', {
+      day: 'numeric',
+      month: 'long',
+    });
+
+    const endOfWeekFormatted = endOfWeek.toLocaleDateString('pl', {
+      day: 'numeric',
+      month: 'long',
+    });
+
+    return `${startOfWeekFormatted} - ${endOfWeekFormatted}`;
+  });
+}
